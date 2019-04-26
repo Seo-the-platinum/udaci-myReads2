5b58e1fcef4c38195aa74923036f04c29ce7169b
eliseo flores <eliseo_flores530@yahoo.com>
2019-04-26 02:06:08 +0000

    fix: Fix delayed input issue on search field by using setstate callback fn

--- src/Search.js
+++ src/Search.js
@@ -26,30 +26,31 @@ componentDidMount() {
    )
 }
 handleChange= (e)=> {
- const query= e
- this.setState(currState=> ({query: query}))
-  if(this.state.query==='' || this.state.query=== undefined){
+  console.log(e)
+ 
+ this.setState((currState)=> ({query: e.trim()}), ()=>{this.search(e)})
+  let { query }= this.state
+  
+}
+
+search= (query)=> {
+  if(query==='' || query=== undefined){
     this.setState(currState=> ({
       searchedBooks: [],
-    }))
- }
-
-  BooksAPI.search(this.state.query).then(data=> {
-   console.log(this.state.query)
+    }))}
+   BooksAPI.search(query).then(data=> {
+    
     data.forEach(b=> { 
       let m= this.state.bookCase.filter(B=> B.id === b.id)
-      if(this.state.query === query){
       if(m[0]) {
         b.shelf=m[0].shelf
       } return this.setState(currState=> ({searchedBooks: data}))
-      }})
+      })
   })
   .catch(error=> {console.log(error)
-       this.setState(currState=> ({searchedBooks: []}))
+       this.setState(currState=> ({ searchedBooks: [] }))
         })
 }
-    
-
 /*update books takes 3 arguments, a book obj, a shelf value from the select element in the book component, and 
 the return data from the update promise. we declare newBooks and set its value to the returned array from the map method
 we passed our bookCase array through. 
